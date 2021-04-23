24e73b8ee1ea31a85c09efdcdd43cbf173d8b54b
Ignacio <dev.ignacio.h@gmail.com>
2021-04-23 11:04:50 -0400

    end

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed quibusdam
+      doloribus sint dolorum, quaerat veniam similique explicabo quis numquam
+      vel? Veniam magni libero corrupti enim nulla? Veniam, rerum! Autem,
+      temporibus!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed quibusdam
+      doloribus sint dolorum, quaerat veniam similique explicabo quis numquam
+      vel? Veniam magni libero corrupti enim nulla? Veniam, rerum! Autem,
+      temporibus!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed quibusdam
+      doloribus sint dolorum, quaerat veniam similique explicabo quis numquam
+      vel? Veniam magni libero corrupti enim nulla? Veniam, rerum! Autem,
+      temporibus!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed quibusdam
+      doloribus sint dolorum, quaerat veniam similique explicabo quis numquam
+      vel? Veniam magni libero corrupti enim nulla? Veniam, rerum! Autem,
+      temporibus!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed quibusdam
+      doloribus sint dolorum, quaerat veniam similique explicabo quis numquam
+      vel? Veniam magni libero corrupti enim nulla? Veniam, rerum! Autem,
+      temporibus!
+    </p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="home">
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-2"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+    ></v-text-field>
+    <v-list flat class="pt-0">
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{ ' text-decoration-line-through': task.done }"
+                >{{ task.title }}</v-list-item-title
+              >
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn icon @click.stop="deleteTask(task.id)">
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  data: () => ({
+    newTaskTitle: '',
+    tasks: [/* 
+      { id: 1, title: 'Comer', done: true },
+      { id: 2, title: 'Comprar', done: false },
+      { id: 3, title: 'Dormir', done: false },
+      { id: 4, title: 'Jugar', done: false },
+      { id: 5, title: 'Tomar', done: false },
+      { id: 6, title: 'Pasear al perro', done: false }
+     */]
+  }),
+  methods: {
+    addTask() {
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ''
+    },
+    doneTask(id) {
+      let task = this.tasks.find((task) => task.id === id);
+      task.done = !task.done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+      console.log(this.tasks);
+    }
+  }
+};
+</script>
